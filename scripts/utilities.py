174059f35cdadea5696234750740465965bfc512
georgeruss <glr09721@uga.edu>
2024-01-30 11:22:20 -0500

    load image bug fix

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -3,6 +3,6 @@ import pygame
 BASE_IMG_PATH = 'data/images/'
 
 def load_image(path):
-    img = pygame.load_image(BASE_IMG_PATH + path).convert()
+    img = pygame.image.load(BASE_IMG_PATH + path).convert()
     img.set_colorkey((0,0,0))
     return img
