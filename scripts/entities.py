423b82fd327a8f285e28c38cdfa497316f708574
georgeruss <landynrussell2019@gmail.com>
2024-01-30 14:58:52 -0500

    player velocity and gravity

--- scripts/entities.py
+++ scripts/entities.py
@@ -12,8 +12,11 @@ class PhysicsEntity:
 
    def update(self, movement=(0, 0)):
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
         self.pos[0] += frame_movement[0]
         self.pos[1] += frame_movement[1]
+
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
 
    def render(self, surf):
        surf.blit(self.game.assets['player'], self.pos)
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,5 +1,6 @@
 class Tilemap:
-    def __init__(self, tile_size = 16):
+    def __init__(self, game, tile_size=16):
+        self.game = game
         self.tile_size = tile_size
         self.tilemap = {}
         self.offgrid_tiles = []
