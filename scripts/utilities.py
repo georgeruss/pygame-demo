e38104ae47cd652feb55b250eea393de57869100
georgeruss <landynrussell2019@gmail.com>
2024-01-30 14:39:45 -0500

    bug fix for util

--- scripts/utilities.py
+++ scripts/utilities.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 BASE_IMG_PATH = 'data/images/'
 
@@ -9,6 +10,6 @@ def load_image(path):
 
 def load_images(path):
     images = []
-    for img_name in os.listdir(BASE_IMG_PATH + path):
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
         images.append(load_image(path + '/' + img_name))
     return images
