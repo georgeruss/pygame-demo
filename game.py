f0bdf2bb88e468e4b8d4e284d8b056b2f17db0b5
georgeruss <glr09721@uga.edu>
2024-01-30 10:38:26 -0500

    functions established

--- game.py
+++ game.py
@@ -1,19 +1,23 @@
 import pygame
 import sys
 
-pygame.init()
+class Game:
+    # initialize game function
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('pygame platformer demo - George Landyn Russell')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
 
-pygame.display.set_caption('pygame platformer demo - George Landyn Russell')
-
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    pygame.display.update()
-    clock.tick(60)
+    # run game function
+    def run(self):
+        while True:
+            self.screen.blit(self.img, (100, 100))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+            self.clock.tick(60)
 
