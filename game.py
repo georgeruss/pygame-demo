b59b328f1af30d86bb8475600623359e7c3bb10a
georgeruss <glr09721@uga.edu>
2024-01-30 11:01:36 -0500

    layering order

--- game.py
+++ game.py
@@ -21,14 +21,15 @@ class Game:
     def run(self):
         while True:
             self.screen.fill((14, 219, 248))
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-            self.screen.blit(self.img, self.img_pos)
 
             img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
             if img_r.colliderect(self.collision_area):
                 pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
             else:
                 pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.screen.blit(self.img, self.img_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
