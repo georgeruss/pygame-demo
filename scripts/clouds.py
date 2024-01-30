ba4bc442a220decdcaa57a110d404bb9bf7ff49a
georgeruss <landynrussell2019@gmail.com>
2024-01-30 16:10:46 -0500

    cloud file update

--- scripts/clouds.py
+++ scripts/clouds.py
@@ -24,3 +24,11 @@ class Clouds:
             self.clouds.append(Cloud(random.random() * 99999, random.random() * 99999, random.choice(cloud_images), random.random() * 0.05 + 0.05, random.random() * 0.6 + 0.2))
         
         self.clouds.sort(key=lambda x: x.depth)    
+
+    def update(self):
+        for cloud in self.clouds:
+            cloud.update()    
+
+    def render(self, surf, offset=(0,0)):
+        for cloud in self.clouds:
+            cloud.render(surf, offset=offset)     
