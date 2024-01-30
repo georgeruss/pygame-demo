2a2d1afee3a17a978421cb3f869901f7e632b669
georgeruss <landynrussell2019@gmail.com>
2024-01-30 16:05:51 -0500

    clouds file added

---
+++ scripts/clouds.py
@@ -0,0 +1,17 @@
+import pygame
+import random
+
+class Cloud:
+    def __init__ (self, pos, img, speed, depth):
+        self.pos = list(pos)
+        self.img = img
+        self.speed = speed
+        self.depth = depth
+    
+    def update(self):
+        self.pos[0] += self.speed
+
+
+    def render(self, surf, offset=(0,0)):
+        render_pos = (self.pos[0] - offset[0] * self.depth, self.pos[1] -  offset[1] * self.depth)
+        surf.blit(self.img, (render_pos[0] % (surf.get_width() + self.img.get_width() - self.img.get_width(), render_pos[1] % (surf.get_height() + self.img.get_height() - self.img.get_height()))))
