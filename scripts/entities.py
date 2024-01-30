f268701f58b9850ae36f8a9e51f6b6e7c8a444ee
georgeruss <glr09721@uga.edu>
2024-01-30 11:21:13 -0500

    entities indentation fix

--- scripts/entities.py
+++ scripts/entities.py
@@ -10,10 +10,10 @@ class PhysicsEntity:
       self.size = size
       self.velocity = [0, 0]  
 
-    def update(self, movement=(0, 0)):
+   def update(self, movement=(0, 0)):
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
         self.pos[0] += frame_movement[0]
         self.pos[1] += frame_movement[1]
 
-    def render(self, surf):
+   def render(self, surf):
        surf.blit(self.game.assets['player'], self.pos)
