34681e14c0c760dc638bdcc929875ff7efa6f09a
georgeruss <glr09721@uga.edu>
2024-01-30 10:52:48 -0500

    bug fix input

--- game.py
+++ game.py
@@ -42,10 +42,10 @@ class Game:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_w:
                         self.movement[0] = True
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_s:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_w:
                         self.movement[0] = False
                     if event.key == pygame.K_s:
                         self.movement[1] = False
