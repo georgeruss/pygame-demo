1879f6c5d8977ca72b4e995d5938262127eeb222
georgeruss <landynrussell2019@gmail.com>
2024-01-30 15:11:50 -0500

    tiles around

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -12,7 +12,13 @@ class Tilemap:
             self.tilemap['10;' + str(i + 5)] = {'type' : 'stone', 'variant': 1, 'pos': (10, 5 + i)}
 
     def tiles_around(self, pos):
-        tile_loc = (int(pos[0] // self.tile_size))
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size) )
+        for offset in NEIGHBOR_OFFSET:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles            
 
     def render(self, surf):
         for tile in self.offgrid_tiles:
