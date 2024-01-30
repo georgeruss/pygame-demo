e8063305d238dd73d2a9b035ce1ffd417cb97e25
georgeruss <landynrussell2019@gmail.com>
2024-01-30 15:02:08 -0500

    neighbord offset

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,3 +1,5 @@
+NEIGHBOR_OFFSET = [(-1,0),(-1,-1),(0,-1),(1,-1),(1,0),(0,0),(-1,1),(0,1), (1,1)]
+
 class Tilemap:
     def __init__(self, game, tile_size=16):
         self.game = game
@@ -8,6 +10,9 @@ class Tilemap:
         for i in range(10):
             self.tilemap[str(3 + i) + ';10'] = {'type' : 'grass', 'variant': 1, 'pos': (3 + i, 10)}
             self.tilemap['10;' + str(i + 5)] = {'type' : 'stone', 'variant': 1, 'pos': (10, 5 + i)}
+
+    def tiles_around(self, pos):
+        tile_loc = (int(pos[0] // self.tile_size))
 
     def render(self, surf):
         for tile in self.offgrid_tiles:
