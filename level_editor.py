87d2082aa52cdae9079a2d06a6c19da6b7fbf682
georgeruss <glr09721@uga.edu>
2024-01-31 18:22:44 -0500

    movement and item selection

--- level_editor.py
+++ level_editor.py
@@ -29,16 +29,51 @@ class Editor:
     
         self.scroll = [0,0]
 
+        self.tile_list = list(self.assets)
+        self.tile_group = 0
+        self.tile_variant = 0
+
+        self.clicking = False
+        self.right_clicking = False
+        self.shift = False
+
     # run game function
     def run(self):
         while True:
             self.display.fill((0,0,0))
 
+            current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
+            current_tile_img.set_alpha(100)
+
+            self.display.blit(current_tile_img, (5, 5))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.clicking = True
+                    if event.button == 3:
+                        self.right_clicking = True
+                    if self.shift:    
+                        if event.button == 4:
+                            self.tile_variant = (self.tile_variant - 1) % len(self.assets[self.tile_list[self.tile_group]])
+                        if event.button == 5:
+                            self.tile_variant = (self.tile_variant + 1) % len(self.assets[self.tile_list[self.tile_group]])
+                    else:
+                        if event.button == 4:
+                            self.tile_group = (self.tile_group - 1) % len(self.tile_list)
+                            self.tile_variant = 0
+                        if event.button == 5:
+                            self.tile_group = (self.tile_group + 1) % len(self.tile_list)   
+                            self.tile_variant = 0
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.clicking = False
+                    if event.button == 3:
+                        self.right_clicking = False
+                        
                 # arrow keys
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
@@ -48,7 +83,9 @@ class Editor:
                     if event.key == pygame.K_UP:
                         self.movement[2] = True
                     if event.key == pygame.K_DOWN:
-                        self.movement[3] = True    
+                        self.movement[3] = True  
+                    if event.key == pygame.K_LSHIFT or pygame.K_RSHIFT:
+                        self.shift = True         
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
@@ -58,6 +95,8 @@ class Editor:
                         self.movement[2] = False
                     if event.key == pygame.K_DOWN:
                         self.movement[3] = False
+                    if event.key == pygame.K_LSHIFT or pygame.K_RSHIFT:
+                        self.shift = False       
 
                 # WASD keys
                 if event.type == pygame.KEYDOWN:
